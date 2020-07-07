a4d956e0401d3aa664b0555cc82b47a4b2645da6
arnie b <arnold.bwaila@gmail.com>
2020-07-07 16:41:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ class App < Sinatra::Base
   
   post '/team' do 
    @name = params[:name]
-   # @coach = params[:coach]
-   # @pg = params[:pg]
-   # @sg = params[:sg
-   # @sf = params[:sf]
-   # @pf = params[:pf]
-   # @c = params[:c]
+   @coach = params[:coach]
+   @pg = params[:pg]
+   @sg = params[:sg
+   @sf = params[:sf]
+   @pf = params[:pf]
+   @c = params[:c]
     puts params.to_s
     erb :team
   end
